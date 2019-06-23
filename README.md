c523c326ee32f3e38962826b28773af1338a9af7
Daniel Vidonyi <daniel.vidonyi@googlemail.com>
2019-06-23 18:18:27 +0800

    Now it's asking for missing parameters; general improvements; including csv default file with examples

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# smart-snippets a.k.a Dave
+# smart-snippets a.k.a. Dave
 Simple &amp; intuitive command line utility to store cheat sheet snippets
--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-tar -zcvf archive/smart-snippets.tar.gz dave
+tar -zcvf archive/smart-snippets.tar.gz dave dave.csv
+
+# Generating hash for brew tap distribution
 echo -e "The SHA256 hash is:"
-shasum -a 256 smart-snippets.tar.gz | awk '{printf $1}'
+shasum -a 256 archive/smart-snippets.tar.gz | awk '{printf $1}'
 echo ""
